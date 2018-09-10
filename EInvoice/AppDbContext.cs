532be8237254fa145154ccf533007dd8645ed13d
DESKTOP-J3R0FDH\lam tuyen <tuyenlqvnp@gmail.com>
2018-09-10 22:19:37 +0700

    add more project

---
+++ EInvoice/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using EInvoice.Models;
+            
+namespace EInvoice
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Item> Item { get; set; }
+        //public DbSet<Customer> Customers { get; set; }
+        //public DbSet<Access> Access { get; set; }
+        //public DbSet<AccessDetail> AccessDetail { get; set; }
+        //public DbSet<Account> Account { get; set; }
+        //public DbSet<Certificate> Certificate { get; set; }
+        //public DbSet<CertificateDetail> CertificateDetail { get; set; }
+        //public DbSet<Company> Company { get; set; }
+        //public DbSet<Config> Config { get; set; }
+        //public DbSet<ConfigDetail> ConfigDetail { get; set; }
+        //public DbSet<Database> DatabaseDB { get; set; }
+        //public DbSet<Invoice> Invoice { get; set; }
+        //public DbSet<InvoiceDetail> InvoiceDetail { get; set; }
+        //public DbSet<InvoiceFormPublish> InvoiceFormPublish { get; set; }
+        //public DbSet<Mail> Mail { get; set; }
+        //public DbSet<NotifyPublishInvoice> NotifyPublishInvoice { get; set; }
+        
+    }
+}
---
+++ EInvoice/Models/Certificate.cs
@@ -0,0 +1,20 @@
+using System;
+namespace EInvoice.Models
+{
+    public class Certificate
+    {
+        private int _id;
+        private int _id_company;
+        private int _id_detail_certificate;
+        private DateTime _create_date;
+
+        public Certificate()
+        {
+        }
+
+        public int Id { get => _id; set => _id = value; }
+        public int Id_company { get => _id_company; set => _id_company = value; }
+        public int Id_detail_certificate { get => _id_detail_certificate; set => _id_detail_certificate = value; }
+        public DateTime Create_date { get => _create_date; set => _create_date = value; }
+    }
+}
